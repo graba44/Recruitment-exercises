6039b0f57a72b0d88ac4421ca98e7df1984772ec
unknown <grabowsky44@gmail.com>
2019-07-03 20:16:09 +0200

    Next 2 exercises have been added

--- 5.js
+++ 5.js
@@ -21,11 +21,55 @@ Notice that lowercase letters are equal to uppercase letters and vice-versa.
 
 const isPalindrome = (string) => {
     string = string.toLowerCase().replace(/[^a-z]+/g, " ");
-    let check = (string === string.split("").reverse().join("")) ? console.log("YES" ) : console.log("NO")
+    let check = (string === string.split("").reverse().join("")) ? "YES" : "NO";
+    console.log(check)
 }
-isPalindrome("abut-1-tuba")
+isPalindrome("abut-1-tuba");
 isPalindrome("@allula");
 
 /* EXERCISE 6 - Permutation
 Write a program that takes two 11 integer array elements (one input per line) and gives output YES, if there's a permutation of the first array that is equal to the second array or gives output NO, if there's no such permutation.
 */
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const array2 = [6, 5, 4, 3, 2, 0, 11, 10, 9, 8, 7];
+
+function checkPermutation (firstArray, secondArray) {
+
+    const sort = (array) => array.sort();
+    const numbersInFirstArray = sort(firstArray);
+    const numbersInSecondArray = sort(secondArray);
+
+    const compare = (firstArray, secondArray) => {
+        if(firstArray.length !== secondArray.length)
+        return false;
+
+        for(let i = firstArray.length; i--;) {
+            if(firstArray[i] !== secondArray[i])
+            return false;
+        } return true;
+    }
+
+    const numberResults = compare(numbersInFirstArray, numbersInSecondArray);
+
+    if(numberResults){
+        return "YES";
+    } return "NO";
+}
+
+console.log(checkPermutation(array1, array2));
+
+/* EXERCISE 9 - Optional Power
+Write a function in JS optionalPow that will receive two numeric arguments. The function is required to create some confirmation logic, where if the user agrees with the question (presses OK), then the function is required to compute the result of raising the first argument to the power determined by the second argument. If the user does not agree with the question (presses Cancel), then it should compute the power of the second argument raised to the first one. In both cases, the function must return the computed value.
+
+For example, optionalPow(2, 3) should return 8 if OK is pressed, otherwise it should return 9.
+*/
+
+let optionalPow;
+(optionalPow = function(num1, num2){
+    if (confirm("Press a button!")) {
+        return Math.pow(num1, num2)
+    } else return Math.pow(num2, num1)
+})();
+
+console.log(optionalPow(2, 5))
